4cea50da5062ee0fd2f1c36618315019e10fe2b3
Guilherme Monteiro de Godoi <101670390+jordf00@users.noreply.github.com>
2026-07-14 23:12:08 -0300

    'aaaa'

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,11 @@
 describe('Login', ()=>{
-    it('Passes', ()=>{
+    it('Valid Login', ()=>{
         cy.viewport('iphone-8')
         cy.visit('https://www.saucedemo.com/')
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.contains('input','Login').click()
+        //cy.get('#login-button').click()
     })
     
 })
