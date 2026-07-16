e28edc2b422f985f1ffcc0ec263d071fc7307f6f
Guilherme Monteiro de Godoi <101670390+jordf00@users.noreply.github.com>
2026-07-15 22:59:44 -0300

    'login'

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,12 @@
 describe('Login', ()=>{
     it('Valid Login', ()=>{
-        cy.viewport('iphone-8')
+        cy.viewport(1024, 768)
         cy.visit('https://www.saucedemo.com/')
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.contains('input','Login').click()
         //cy.get('#login-button').click()
+        cy.contains('Swag Labs').should('be.visible')
     })
     
 })
